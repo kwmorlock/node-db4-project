55c6785842d6e597c51a1203de09b4fdbafdb472
Kenneth Morlock <kennethmorlock@Kenneths-MBP.fios-router.home>
2020-05-14 16:57:15 -0400

    server updated for recipes

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 
-const SchemeRouter = require('./schemes/scheme-router.js');
+const RecipesRouter = require('./recipes/recipes-router.js');
 
 const server = express();
 
 server.use(express.json());
-server.use('/api/schemes', SchemeRouter);
+server.use('/api/recipes', RecipesRouter);
 //line 9 is starting point for site to use in postman
 
 module.exports = server;
